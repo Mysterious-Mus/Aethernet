669c1c97582514bb148c72c5e05330fab0e26015
Zaizhou <1206315053@qq.com>
2023-12-12 23:49:26 +0800

    add ttl to arp entry

--- src/main/java/com/aethernet/Aethernet/ARPTable.java
+++ src/main/java/com/aethernet/Aethernet/ARPTable.java
@@ -1,6 +1,13 @@
 package com.aethernet.Aethernet;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.management.Notification;
 
 import org.pcap4j.packet.Packet;
 
@@ -8,10 +15,17 @@ import java.util.HashMap;
 import java.net.Inet4Address;
 import com.aethernet.Aethernet.utils.IPAddr;
 import com.aethernet.Aethernet.utils.PacketResolve;
+import com.aethernet.utils.ANSI;
+import com.aethernet.utils.sync.Notifier;
 
 public class ARPTable {
 
+    public static class Configs {
+        public static int EXPIRE_TIME = 15;  // seconds
+    }
+
     Map<Inet4Address, Byte> table;
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     public ARPTable() {
         table = new HashMap<Inet4Address, Byte>();
@@ -36,5 +50,12 @@ public class ARPTable {
     public void addEntry(Inet4Address ipaddr, Byte macaddr) {
         // if already present, overwrite
         table.put(ipaddr, macaddr);
+        // cancel the previous delete task if it exists
+        // schedule a new delete task
+        executor.schedule(() -> {
+            table.remove(ipaddr);
+            System.out.println(ANSI.ANSI_CYAN +"ARPTable: entry " + ipaddr + " deleted"+ANSI.ANSI_RESET);
+        }, Configs.EXPIRE_TIME, TimeUnit.SECONDS);
     }
+
 }
